0afdb0b39cdbf7fbb610cc4c1f5e13a07ed43a26
Gonzako <al341522@uji.es>
2020-01-10 21:09:10 +0100

    added draft of gamepaly

---
+++ Assets/C#Scripts/BasePlayerController.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class BasePlayerController : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if true
+    #region Unity API
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
---
+++ Assets/Scripts/rotateTowardsMouse.cs
@@ -0,0 +1,58 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+
+using System;
+using UnityEngine;
+
+public class rotateTowardsMouse : MonoBehaviour
+{
+    #region Public Fields
+    public int maxRotateAngle = 15, minRotateAngle = 5;
+    #endregion
+
+    #region Private Fields
+    Camera mainCam;
+
+    #region GarbaseSabers
+    Vector3 screenPos;
+    #endregion
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+
+
+    private void rotateToMousePos()
+    {
+        screenPos = mainCam.WorldToScreenPoint(transform.position);
+        Vector3 directionVect = Input.mousePosition - screenPos;
+        float angle = Mathf.Atan2(directionVect.y, directionVect.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+    }
+    #endregion
+
+
+#if true
+    #region Unity API
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+ 
+    void Update()
+    {
+        rotateToMousePos();
+    }
+         
+    #endregion
+#endif
+
+}
