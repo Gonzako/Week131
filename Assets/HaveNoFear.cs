cf48f6442b100e0315a069e47c5f3f964b30446c
Eric Pratt <ericprattusc@gmail.com>
2020-01-10 13:54:15 -0800

    Have no fear

---
+++ Assets/HaveNoFear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaveNoFear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Have no fear, Jon is here"); 
+        
+    }
+}
