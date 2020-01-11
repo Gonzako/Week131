997ab07b8d5f9f121a6b60b08b6f95e07b4ae49d
Gonzako <al341522@uji.es>
2020-01-11 13:55:52 +0100

    dinner commit

---
+++ Assets/gunManager.cs
@@ -0,0 +1,41 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class gunManager : MonoBehaviour
+{
+    #region Public Fields
+    public BasicScriptableMask mask;
+    public Transform firingPosition;
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if false
+    #region Unity API
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
---
+++ Assets/maskAnimator.cs
@@ -0,0 +1,45 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class maskAnimator : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    [SerializeField]
+    GameObject frontMask, backMask, leftMask, rightMask;
+    #endregion
+
+    #region Public Methods
+    public void updateMask(BasicScriptableMask data)
+    {
+
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if false
+    #region Unity API
+
+    void Start()
+    {
+    }
+ 
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
