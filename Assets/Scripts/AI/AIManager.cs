1770f86d1787c403f5315ea76f3eb571ab629a25
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-01-11 16:09:32 +0200

    functional AI

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Pathfinding;
 
 public class AIManager : MonoBehaviour
 {
     private Dictionary<Type, BaseAIState> _initialStates;
+    private AIStateManager _aistatemng;
+    public AIDestinationSetter _trgmngr;
+
+    
 
     private void OnEnable()
     {
         SetupStates();
     }
 
+    private void Start()
+    {
+        SetupStates();
+        _trgmngr = GetComponent<AIDestinationSetter>();
+    }
+
     private void SetupStates()
     {
+        _aistatemng = GetComponent<AIStateManager>();
         _initialStates = new Dictionary<Type, BaseAIState>
         {
             {typeof(ChaseState), new ChaseState(this)}
         };
+        _aistatemng.SetStates(_initialStates);
     }
 }
--- Assets/Scripts/AI/States/ChaseState.cs
+++ Assets/Scripts/AI/States/ChaseState.cs
@@ -1,18 +1,28 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChaseState : BaseAIState
-{ 
-    public  ChaseState(AIManager ai) : base(ai)
+{
+    public ChaseState(AIManager ai) : base(ai)
     {
         _ai = ai;
     }
+
     public override AIManager _ai { get; set; }
+
+    private GameObject _Player;
+
+    public override void OnStateEnter()
+    {
+        _Player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     public override Type Tick()
     {
-        throw new NotImplementedException();
+
+        _ai._trgmngr.target = _Player.transform;
+        
+  
+        return null;
     }
 }
