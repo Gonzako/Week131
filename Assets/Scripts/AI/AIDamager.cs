829ce9b0f51656c4e9486de88a485decaaf4bb0f
Gonzako <al341522@uji.es>
2020-01-15 23:45:13 +0100

    made gorillas recoil when hitting player

--- Assets/Scripts/AI/AIDamager.cs
+++ Assets/Scripts/AI/AIDamager.cs
@@ -6,6 +6,7 @@ public class AIDamager : MonoBehaviour
 {
 
     [SerializeField] private int _damage;
+    [SerializeField] private float forceMultiplier = 10;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,7 +14,9 @@ public class AIDamager : MonoBehaviour
         {
             IMortal m = collision.transform.GetComponent<IMortal>();
             collision.otherRigidbody.AddForce(collision.
-                GetContact(0).normal * _damage, ForceMode2D.Impulse);
+                GetContact(0).normal * _damage * forceMultiplier, ForceMode2D.Impulse);
+            collision.rigidbody.AddForce(collision.GetContact(0).normal *
+                _damage * forceMultiplier, ForceMode2D.Impulse);
             Damage(m);
         }
     }
