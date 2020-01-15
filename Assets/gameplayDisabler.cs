687ed93b447bc94a63df716cb2b7eda494639163
Gonzako <al341522@uji.es>
2020-01-15 23:38:25 +0100

    made player disable on shop open

---
+++ Assets/gameplayDisabler.cs
@@ -0,0 +1,56 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+
+using System;
+using UnityEngine;
+
+public class gameplayDisabler : MonoBehaviour
+{
+    #region Public Fields
+    public GameObject playerGuy;
+    #endregion
+ 
+    #region Private Fields
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if true
+    #region Unity API
+
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        ShopInteractable.onAnyOpenCommand += disableOrEnable;
+    }
+
+    private void OnDisable()
+    {
+        ShopInteractable.onAnyOpenCommand -= disableOrEnable;
+    }
+
+    private void disableOrEnable()
+    {
+        playerGuy.SetActive(!playerGuy.activeSelf);
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    #endregion
+    #endif
+ 
+}
