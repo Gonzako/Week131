e772c49c8f0887431a2105691a7998efa0744ca3
Gonzako <al341522@uji.es>
2020-01-16 14:50:32 +0100

    made cinemachine look at mouse

---
+++ Assets/followMouse.cs
@@ -0,0 +1,44 @@
+﻿/*
+ *
+ * Copyright (c) Gonzako
+ * Gonzako123@gmail.com
+ *
+ */
+ 
+using UnityEngine;
+ 
+public class followMouse : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    private Camera cam;
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+
+    #if true
+    #region Unity API
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+ 
+    void FixedUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Mathf.Abs(cam.transform.position.z);
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    #endregion
+    #endif
+ 
+}
